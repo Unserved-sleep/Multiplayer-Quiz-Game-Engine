5f842e936923894017ab22384a6656324ec2c704
Sidharth S K <87354023+Sidharth-SK@users.noreply.github.com>
2026-04-24 12:01:34 +0530

    changed branch from master to main

---
+++ Multiplayer Quiz Game Engine.py
@@ -0,0 +1,21 @@
+"""
+Quiz engine that has the following features:
+- question banks
+- live scoring
+- leaderboard with streaks
+- badges
+
+
+@Authors:
+    -Sidharth S K
+    -Aditya Kumar
+    -Ashutosh Sharma
+    -Sanskar Dewangan
+@Date: 24-042026
+
+To-Do:
+- Question banks & Catagory System
+- Player & Scoring Engine
+- Leaderboard, badges & stats
+"""
+
